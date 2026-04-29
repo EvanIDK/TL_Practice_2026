eb66d2322019049aab47a2bbbe2293e4e1930f57
Иван <gerasimov_ivan2006@mail.ru>
2026-04-30 01:21:50 +0300

    OrderManager: правки по ревью

---
+++ OrderManager/InputReader.cs
@@ -0,0 +1,48 @@
+﻿internal static class InputReader
+{
+
+    internal static string ReadLettersOnly( string orderInfo )
+    {
+        Console.Write( orderInfo );
+        string input = Console.ReadLine()?.Trim() ?? string.Empty;
+
+        while ( ( input == "" ) || ( !input.All( char.IsLetter ) ) )
+        {
+            Console.WriteLine( "Можно использовать только буквы!" );
+            Console.Write( orderInfo );
+            input = Console.ReadLine()?.Trim() ?? string.Empty;
+        }
+
+        return input;
+    }
+
+    internal static string ReadNonEmpty( string orderInfo )
+    {
+        Console.Write( orderInfo );
+        string input = Console.ReadLine()?.Trim() ?? string.Empty;
+
+        while ( input == "" )
+        {
+            Console.WriteLine( "Поле не может быть пустым!" );
+            Console.Write( orderInfo );
+            input = Console.ReadLine()?.Trim() ?? string.Empty;
+        }
+
+        return input;
+    }
+
+    internal static string ReadQuantityInt( string orderInfo )
+    {
+        Console.Write( orderInfo );
+        string input = Console.ReadLine()?.Trim() ?? string.Empty;
+
+        while ( ( !int.TryParse( input, out int value ) ) || ( value <= 0 ) )
+        {
+            Console.WriteLine( "Введите целое положительное число!" );
+            Console.Write( orderInfo );
+            input = Console.ReadLine()?.Trim() ?? string.Empty;
+        }
+
+        return input;
+    }
+}
---
+++ OrderManager/OrderManager.cs
@@ -0,0 +1,46 @@
+﻿internal class OrderManager
+{
+    private string? _productName;
+    private string? _productQuantity;
+    private string? _userName;
+    private string? _shippingAddress;
+    private DateTime _todayDate = DateTime.Today;
+
+    public void GetClientOrder()
+    {
+        Console.WriteLine( "1. Запрос данных у пользователя " );
+        _productName = InputReader.ReadNonEmpty( " - Название товара: " );
+        _productQuantity = InputReader.ReadQuantityInt( " - Количество товара: " );
+        _userName = InputReader.ReadLettersOnly( " - Имя пользователя: " );
+        _shippingAddress = InputReader.ReadNonEmpty( " - Адрес доставки: " );
+    }
+
+    public OrderResult OrderConfirmation()
+    {
+        Console.WriteLine( $"Здравствуйте, {_userName}, вы заказали {_productQuantity} {_productName} на адрес {_shippingAddress}, все верно?" );
+        Console.WriteLine( "Да/Нет: " );
+        string? userResponse = Console.ReadLine()?.Trim();
+
+        while ( true )
+        {
+            if ( ( userResponse == "Да" ) || ( userResponse == "да" ) )
+            {
+                return OrderResult.Success;
+            }
+            else if ( ( userResponse == "Нет" ) || ( userResponse == "нет" ) )
+            {
+                return OrderResult.RepeatOrder;
+            }
+            else
+            {
+                Console.Write( "Введите Да/Нет" );
+                userResponse = Console.ReadLine()?.Trim();
+            }
+        }
+    }
+
+    public void PrintOrderConfirmation()
+    {
+        Console.WriteLine( $"{_userName}! Ваш заказ {_productName} в количестве {_productQuantity} оформлен! Ожидайте доставку по адресу {_shippingAddress} к {_todayDate.AddDays( 3 ):dd.MM.yyyy}" );
+    }
+}
