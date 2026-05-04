fef46238a98c3d72770defd6db181aeca2665c09
Иван <gerasimov_ivan2006@mail.ru>
2026-05-04 16:10:04 +0300

    Casino: финальная структура папок

---
+++ Casino/Casino/OptionHandleResult.cs
@@ -0,0 +1,7 @@
+﻿internal enum OptionHandleResult
+{
+    Success = 0,
+    InvalidOption = 1,
+    InvalidDepositNumber = 2,
+    InvalidBet = 3,
+}
---
+++ Casino/Casino/Program.cs
@@ -0,0 +1,11 @@
+﻿Casino casino = new Casino();
+
+Casino.PrintHeader();
+casino.MakeDeposit();
+do
+{
+    casino.PrintMenu();
+    string option = Console.ReadLine() ?? string.Empty;
+    casino.HandleOptions( option );
+}
+while ( !casino._isGameFinished );
