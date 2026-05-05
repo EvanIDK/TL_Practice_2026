9ab9de31cd90a1b91a6aaa2e6dffc32b9a84943e
Иван <gerasimov_ivan2006@mail.ru>
2026-05-05 22:02:04 +0300

    CarFactory: базовая реализация

---
+++ CarFactory/CarFactory/Car.cs
@@ -0,0 +1,69 @@
+﻿using Spectre.Console;
+internal class Car
+{
+    private string _make = string.Empty;
+    private string _engine = string.Empty;
+    private string _transmission = string.Empty;
+    private string _kuzov = string.Empty;
+    private string _color = string.Empty;
+    private int _numberOfGears;
+    private int _maxSpeed = 180;
+
+    public void Configure( string make, string engine,
+                         string transmission, string kuzov, string color )
+    {
+        _make = make;
+        _engine = engine;
+        _transmission = transmission;
+        _kuzov = kuzov;
+        _color = color;
+        CalculateStats();
+    }
+
+    private void CalculateStats()
+    {
+        if ( _kuzov == "Седан" )
+        {
+            _maxSpeed += 20;
+        }
+        else if ( _kuzov == "Джип" )
+        {
+            _maxSpeed -= 10;
+        }
+
+        if ( _transmission == "Механика" )
+        {
+            _maxSpeed += 15;
+            _numberOfGears = 6;
+        }
+        else if ( _transmission == "Автомат" )
+        {
+            _numberOfGears = 9;
+        }
+
+        if ( _engine == "V8" )
+        {
+            _maxSpeed += 40;
+        }
+    }
+
+    public void PrintStats()
+    {
+        AnsiConsole.Clear();
+        AnsiConsole.MarkupLine( $"[green]Готово![/] Твоя {_make} собрана!" );
+        AnsiConsole.MarkupLine( $"[yellow]Характеристики: [/]" );
+        AnsiConsole.MarkupLine( $" - Кузов: {_kuzov}" );
+        AnsiConsole.MarkupLine( $" - Цвет: {_color}" );
+        AnsiConsole.MarkupLine( $" - Мотор: {_engine}" );
+        AnsiConsole.MarkupLine( $" - КПП: {_transmission}" );
+        AnsiConsole.MarkupLine( $" - Максимальная скорость: [red]{_maxSpeed}[/] км/ч" );
+        AnsiConsole.MarkupLine( $" Кол-во передач: [red]{_numberOfGears} [/]" );
+    }
+}
+
+
+
+
+
+
+
---
+++ CarFactory/CarFactory/Program.cs
@@ -0,0 +1,14 @@
+﻿using Spectre.Console;
+
+var greeting = new FigletText( "Car Factory" )
+{
+    Justification = Justify.Center
+};
+AnsiConsole.Write( greeting );
+
+var configurator = new CarConfigurator();
+var config = configurator.GetConfiguration();
+
+var car = new Car();
+car.Configure( config.make, config.engine, config.transmission, config.kuzov, config.color );
+car.PrintStats();
